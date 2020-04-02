b8f734b84ea97b7fb059cf55acdeca205dc75781
Dmytro <dimalg400@gmail.com>
2020-04-03 00:39:15 +0300

    wipe

--- app/src/main/java/com/example/dyplommessenger/end_to_end_AES/EndToEndAES.java
+++
@@ -1,4 +0,0 @@
-package com.example.dyplommessenger.end_to_end_AES;
-
-public class EndToEndAES {
-}
--- app/src/main/java/com/example/dyplommessenger/models/ChatMessage.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dyplommessenger.models
-
-class ChatMessage(val id: String, val text: String, val id_from: String, val id_to: String, val timestamp: Long){
-    constructor(): this("", "", "", "", -1)
-}
